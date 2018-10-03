2919eea5f333ea8d6f664dddf4498b7f41d495ca
Ali Rafiee Pour <34745068+AliRafieePour@users.noreply.github.com>
2018-10-03 22:09:14 +0330

    edited some irrelevant comments

--- ANN_Time Series analysis.py
+++ ANN_Time Series analysis.py
@@ -1,259 +1,78 @@
-## -*- coding: utf-8 -*-
-#"""
-#Spyder Editor
-#
-#This is a temporary script file.
-#"""
-#
-#import tensorflow as ts
-#import numpy as np
-#import numpy
-#import matplotlib.pyplot as plt
-#from pandas import read_csv
-#import math
-#from keras.models import Sequential
-#from keras.layers import Dense
-#from keras.utils.generic_utils import get_custom_objects
-#from keras import backend as k
-#def swish(x):
-#    return (k.sigmoid(x) * x)
-#
-#get_custom_objects().update({'swish': swish})
-#
-## convert an array of values into a dataset matrix
-#def create_dataset(dataset, look_back=1):
-#	dataX, dataY = [], []
-#	for i in range(len(dataset)-look_back-1):
-#		a = dataset[i:(i+look_back), 0]
-#		dataX.append(a)
-#		dataY.append(dataset[i + look_back, 0])
-#	return numpy.array(dataX), numpy.array(dataY)
-#
-## fix random seed for reproducibility
-#numpy.random.seed(7)
-## load the dataset
-#
-#
-#filename = "aa2.csv"
-#dataframe = read_csv(filename, usecols=[1], engine='python', skipfooter=3)
-#dataset = dataframe.values
-#dataset = dataset.astype('float32')
-## split into train and test sets
-#train_size = int(len(dataset) * .7)
-#test_size = len(dataset) - train_size
-#train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-## reshape dataset
-#look_back = 1
-#trainX, trainY = create_dataset(train, look_back)
-#testX, testY = create_dataset(test, look_back)
-#
-## create and fit Multilayer Perceptron model
-#model = Sequential()
-#model.add(Dense(15, input_dim=look_back, activation='swish'))
-##model.add(Dense(15, activation='relu'))
-#model.add(Dense(1))
-#model.compile(loss='mean_squared_error', optimizer='SGD')
-#model.fit(trainX, trainY, epochs=100, batch_size=2, verbose=2)
-## Estimate model performance
-#trainScore = model.evaluate(trainX, trainY, verbose=0)
-#print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore, math.sqrt(trainScore)))
-#testScore = model.evaluate(testX, testY, verbose=0)
-#print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore, math.sqrt(testScore)))
-## generate predictions for training
-#trainPredict = model.predict(trainX)
-#testPredict = model.predict(testX)
-## shift train predictions for plotting
-#trainPredictPlot = numpy.empty_like(dataset)
-#trainPredictPlot[:, :] = numpy.nan
-#trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-## shift test predictions for plotting
-#testPredictPlot = numpy.empty_like(dataset)
-#testPredictPlot[:, :] = numpy.nan
-#testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-## plot baseline and predictions
-#plt.plot(dataset)
-#plt.plot(trainPredictPlot)
-#plt.plot(testPredictPlot)
-#plt.show()
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tensorflow as ts
+import numpy as np
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils.generic_utils import get_custom_objects
+from keras import backend as k
+def swish(x):
+    return (k.sigmoid(x) * x)
+
+get_custom_objects().update({'swish': swish})
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+# load the dataset
 
 
-import numpy as np, random, operator, pandas as pd, matplotlib.pyplot as plt
+filename = "aa2.csv"
+dataframe = read_csv(filename, usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+# split into train and test sets
+train_size = int(len(dataset) * .7)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+# reshape dataset
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
 
+# create and fit Multilayer Perceptron model
+model = Sequential()
+model.add(Dense(15, input_dim=look_back, activation='swish'))
+#model.add(Dense(15, activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='SGD')
+model.fit(trainX, trainY, epochs=100, batch_size=2, verbose=2)
+# Estimate model performance
+trainScore = model.evaluate(trainX, trainY, verbose=0)
+print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore, math.sqrt(trainScore)))
+testScore = model.evaluate(testX, testY, verbose=0)
+print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore, math.sqrt(testScore)))
+# generate predictions for training
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(dataset)
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
 
-class City:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def distance(self, city):
-        xDis = abs(self.x - city.x)
-        yDis = abs(self.y - city.y)
-        distance = np.sqrt((xDis ** 2) + (yDis ** 2))
-        return distance
-    
-    def __repr__(self):
-        return "(" + str(self.x) + "," + str(self.y) + ")"
-
-
-def initialPopulation(popSize, cityList):
-    population = []
-
-    for i in range(0, popSize):
-        population.append(createRoute(cityList))
-    return population
-
-
-class Fitness:
-    def __init__(self, route):
-        self.route = route
-        self.distance = 0
-        self.fitness= 0.0
-    
-    def routeDistance(self):
-        if self.distance ==0:
-            pathDistance = 0
-            for i in range(0, len(self.route)):
-                fromCity = self.route[i]
-                toCity = None
-                if i + 1 < len(self.route):
-                    toCity = self.route[i + 1]
-                else:
-                    toCity = self.route[0]
-                pathDistance += fromCity.distance(toCity)
-            self.distance = pathDistance
-        return self.distance
-    
-    def routeFitness(self):
-        if self.fitness == 0:
-            self.fitness = 1 / float(self.routeDistance())
-        return self.fitness
-    
-
-
-def createRoute(cityList):
-    route = random.sample(cityList, len(cityList))
-    return route
-
-def rankRoutes(population):
-    fitnessResults = {}
-    for i in range(0,len(population)):
-        fitnessResults[i] = Fitness(population[i]).routeFitness()
-    return sorted(fitnessResults.items(), key = operator.itemgetter(1), reverse = True)
-
-
-def selection(popRanked, eliteSize):
-    selectionResults = []
-    df = pd.DataFrame(np.array(popRanked), columns=["Index","Fitness"])
-    df['cum_sum'] = df.Fitness.cumsum()
-    df['cum_perc'] = 100*df.cum_sum/df.Fitness.sum()
-    
-    for i in range(0, eliteSize):
-        selectionResults.append(popRanked[i][0])
-    for i in range(0, len(popRanked) - eliteSize):
-        pick = 100*random.random()
-        for i in range(0, len(popRanked)):
-            if pick <= df.iat[i,3]:
-                selectionResults.append(popRanked[i][0])
-                break
-    return selectionResults
-
-
-def matingPool(population, selectionResults):
-    matingpool = []
-    for i in range(0, len(selectionResults)):
-        index = selectionResults[i]
-        matingpool.append(population[index])
-    return matingpool
-
-
-def breed(parent1, parent2):
-    child = []
-    childP1 = []
-    childP2 = []
-    
-    geneA = int(random.random() * len(parent1))
-    geneB = int(random.random() * len(parent1))
-    
-    startGene = min(geneA, geneB)
-    endGene = max(geneA, geneB)
-
-    for i in range(startGene, endGene):
-        childP1.append(parent1[i])
-        
-    childP2 = [item for item in parent2 if item not in childP1]
-
-    child = childP1 + childP2
-    return child
-
-
-def breedPopulation(matingpool, eliteSize):
-    children = []
-    length = len(matingpool) - eliteSize
-    pool = random.sample(matingpool, len(matingpool))
-
-    for i in range(0,eliteSize):
-        children.append(matingpool[i])
-    
-    for i in range(0, length):
-        child = breed(pool[i], pool[len(matingpool)-i-1])
-        children.append(child)
-    return children
-
-
-
-
-def mutate(individual, mutationRate):
-    for swapped in range(len(individual)):
-        if(random.random() < mutationRate):
-            swapWith = int(random.random() * len(individual))
-            
-            city1 = individual[swapped]
-            city2 = individual[swapWith]
-            
-            individual[swapped] = city2
-            individual[swapWith] = city1
-    return individual
-
-
-
-
-def mutatePopulation(population, mutationRate):
-    mutatedPop = []
-    
-    for ind in range(0, len(population)):
-        mutatedInd = mutate(population[ind], mutationRate)
-        mutatedPop.append(mutatedInd)
-    return mutatedPop
-
-
-
-def nextGeneration(currentGen, eliteSize, mutationRate):
-    popRanked = rankRoutes(currentGen)
-    selectionResults = selection(popRanked, eliteSize)
-    matingpool = matingPool(currentGen, selectionResults)
-    children = breedPopulation(matingpool, eliteSize)
-    nextGeneration = mutatePopulation(children, mutationRate)
-    return nextGeneration
-
-
-
-def geneticAlgorithm(population, popSize, eliteSize, mutationRate, generations):
-    pop = initialPopulation(popSize, population)
-    print("Initial distance: " + str(1 / rankRoutes(pop)[0][1]))
-    
-    for i in range(0, generations):
-        pop = nextGeneration(pop, eliteSize, mutationRate)
-    
-    print("Final distance: " + str(1 / rankRoutes(pop)[0][1]))
-    bestRouteIndex = rankRoutes(pop)[0][0]
-    bestRoute = pop[bestRouteIndex]
-    return bestRoute
-
-
-cityList = []
-
-for i in range(0,50):
-    cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
-    
-    
-geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.1, generations=500)
